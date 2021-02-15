4925c774fa01542bb2c39f739f448b4c3a5f6125
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-15 23:59:12 +0100

    Create the object of TextView and find it

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.covid19tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Create the object of TextView Class
+    TextView tvCases;
+    TextView tvRecovered;
+    TextView tvCritical;
+    TextView tvActive;
+    TextView tvTodayCases;
+    TextView tvTotalDeaths;
+    TextView tvTodayDeaths;
+    TextView tvAffectedCountries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the object by id from the xml
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+    }
+
+    //Get the data from 3rd party services
+    private void fetchdata()
+    {
+
     }
 }

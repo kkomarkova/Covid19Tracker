7372fd18a5b1039d80ffb15da11194302e643953
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-16 20:53:48 +0100

    Get data from 3rd party services

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -4,6 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,11 +44,69 @@ public class MainActivity extends AppCompatActivity {
         tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
         tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
         tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+        // Creating a method fetchdata()
+        fetchdata();
     }
 
     //Get the data from 3rd party services
     private void fetchdata()
     {
+        String url = "https:// corona.lmao.ninja/v2/all";
 
+        StringRequest request
+                = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object response)
+                    {
+        // Handle the JSON object and handle it inside try and catch
+
+                        try {
+                            // Creating object of JSONObject
+                            JSONObject jsonObject
+                                    = new JSONObject(
+                                    response.toString());
+
+                            tvCases.setText(
+                                    jsonObject.getString("cases"));
+                            tvRecovered.setText(
+                                    jsonObject.getString("recovered"));
+                            tvCritical.setText(
+                                    jsonObject.getString("critical"));
+                            tvActive.setText(
+                                    jsonObject.getString("active"));
+                            tvTodayCases.setText(
+                                    jsonObject.getString("todayCases"));
+                            tvTotalDeaths.setText(
+                                    jsonObject.getString("deaths"));
+                            tvTodayDeaths.setText(
+                                    jsonObject.getString("todayDeaths"));
+                            tvAffectedCountries.setText(
+                                    jsonObject.getString("affectedCountries"));
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(
+                            VolleyError error)
+                    {
+                        Toast.makeText(MainActivity.this,
+                                error.getMessage(),
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+
+        RequestQueue requestQueue
+                = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }

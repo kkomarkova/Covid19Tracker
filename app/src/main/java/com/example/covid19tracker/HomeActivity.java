b541ae274db84fa4d7bb9b72a246a7e1ff598881
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-16 23:25:35 +0100

    Launch app

---
+++ app/src/main/java/com/example/covid19tracker/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.covid19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+    }
+}

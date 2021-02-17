4d6aa79b6423ab6c3d6f5b762152cbc23c034c66
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-17 12:15:36 +0100

    Country details activity created

---
+++ app/src/main/java/com/example/covid19tracker/CountryDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.covid19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountryDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_details);
+    }
+}

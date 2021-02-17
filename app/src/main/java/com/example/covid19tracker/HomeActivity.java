5b7df72acbce35b9731200a1642c9d24c0226b61
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-02-17 11:49:37 +0100

    Update HomeActivity.java
    Navigate to the MainActivity from Home

--- app/src/main/java/com/example/covid19tracker/HomeActivity.java
+++ app/src/main/java/com/example/covid19tracker/HomeActivity.java
@@ -2,8 +2,10 @@ package com.example.covid19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,5 +15,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         getSupportActionBar().hide();
 
+        //Set display launch app for 3 second
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Go to the MainActivity after 3 seconds
+                {
+                    Intent intent=new Intent(HomeActivity.this,MainActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        }, 3000);
+
+
     }
+
 }
